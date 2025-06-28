4c6dacf10fd9f3df164057e83127000966dc5c1d
Anita Caron <anitacaron@alumni.usp.br>
2025-06-28 11:12:00 -0300

    update docker image to use one most updated from main OBO-Dashboard repo

--- odk.sh
+++ odk.sh
@@ -10,8 +10,8 @@
 #
 # See README-editors.md for more details.
 docker run -e ROBOT_JAVA_ARGS='-Xmx58G' -e JAVA_OPTS='-Xmx58G' \
-  -v $PWD/dashboard:/tools/OBO-Dashboard/dashboard \
-  -v $PWD/dashboard-config.yml:/tools/OBO-Dashboard/dashboard-config.yml \
-  -v $PWD/ontologies:/tools/OBO-Dashboard/build/ontologies \
-  -v $PWD/sparql:/tools/OBO-Dashboard/sparql \
-  -w /work --rm -ti obolibrary/odkfull:dev "$@"
+  -v $PWD/dashboard:/tools/dashboard \
+  -v $PWD/dashboard-config.yml:/tools/dashboard-config.yml \
+  -v $PWD/ontologies:/tools/ontologies \
+  -v $PWD/sparql:/tools/sparql \
+  -w /work --rm -ti anitacaron/obo-dashboard:v0.1 "$@"
